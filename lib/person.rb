b5c777f1a80ed64a3b494a0030bcbec96dda1b9c
Roli <rroycehg@gmail.com>
2021-07-22 11:18:52 -0400

    commit tree 2be018e705e676146edac6a4743aa616aef796ab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # your code goes here
+class Person
+    attr_reader :name, :bank_account
+    
+
+    def initialize(name, bank_account = 25)
+        @name = name
+        @bank_account = bank_account
+    end
+
+    
+end
